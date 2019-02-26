c1a9ab4d7d04c247257e97d9e232120189e75b2f
Sunil Gunisetty <sgunisetty@2u.com>
2019-02-26 14:27:57 -0500

    Tests pass

--- test/net/eraserhead/clara_eql_test.clj
+++ test/net/eraserhead/clara_eql_test.clj
@@ -1,7 +1,6 @@
 (ns net.eraserhead.clara-eql-test
-  (:require [clojure.test :refer :all]
+  (:require [midje.sweet :refer :all]
             [net.eraserhead.clara-eql :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "about generating rules"
+  (+ 2 2) => 4)
