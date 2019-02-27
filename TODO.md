2554f4d18ac924d7b6e76ef8264681b861629f57
Sunil Gunisetty <sgunisetty@2u.com>
2019-02-27 16:17:19 -0500

    Add some future-facts and todo items

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO
+
+- [ ] Move rules/pull and eav-map into this project
+- [ ] Configure CircleCI
+- [ ] Add ability to pass properties through to defrule
+- [ ] Add ability to pass docstring through to defrule
+- [ ] Add a docstring to defrule itself
+- [ ] We should have a predicate variable? which means a symbol which starts with a question mark,
+      for the :from spec.
--- test/net/eraserhead/clara_eql_test.clj
+++ test/net/eraserhead/clara_eql_test.clj
@@ -24,15 +24,43 @@
   :where
   [EAV (= e ?eid) (= a :foo/uuid)])
 
+(defrule many-valued-key
+  :query [:foo/uuid
+          (:foo/many-valued {many-valued? true})]
+  :from ?eid
+  :where
+  [EAV (= e ?eid) (= a :foo/uuid)])
+
 (facts "about parse-rule"
-  (fact "it generates one result for every match of `:where`"
-    (let [session (-> (r/mk-session)
-                      (r/insert (eav/->EAV 10 :foo/uuid "aaa"))
-                      (r/fire-rules))]
-      (r/query session query-results) => (contains {:?query `basic-rule
-                                                    :?root 10
-                                                    :?data {:foo/uuid "aaa"}})
-      (fact "missing top-level properties don't prevent returning a result"
-        (r/query session query-results) => (contains {:?query `missing-property-rule
-                                                      :?root 10
-                                                      :?data {:foo/uuid "aaa"}})))))
+  (let [session (-> (r/mk-session)
+                    (r/insert (eav/->EAV 10 :foo/uuid "aaa")
+                              (eav/->EAV 10 :foo/many-valued 1)
+                              (eav/->EAV 10 :foo/many-valued 2)
+                              (eav/->EAV 20 :foo/uuid "bbb"))
+                    (r/fire-rules))
+        results (r/query session query-results)]
+    (facts "about top-level keys"
+      (facts "about single-cardinality keys"
+        (fact "returns a result when all values are present"
+          results => (contains {:?query `basic-rule
+                                :?root 10
+                                :?data {:foo/uuid "aaa"}}))
+        (fact "returns a result when root is missing a key"
+          results => (contains {:?query `missing-property-rule
+                                :?root 10
+                                :?data {:foo/uuid "aaa"}})))
+      (facts "about cardinality-many keys"
+        (future-fact "returns all values for a cardinality-many key"
+          results => (contains {:?query `many-valued-key
+                                :?root 10
+                                :?data {:foo/uuid "aaa"
+                                        :foo/many-valued [1 2]}}))
+        (future-fact "returns an empty set for a cardinality-many key if no values are present"
+          results => (contains {:?query `many-valued-key
+                                :?root 20
+                                :?data {:foo/uuid "bbb"
+                                        :foo/many-valued []}}))))
+    (facts "about joins"
+      (future-fact "returns joined values"))
+    (facts "about unions"
+      (future-fact "returns values from all branches of the union"))))
