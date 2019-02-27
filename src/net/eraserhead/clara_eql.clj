de4fe31bc9eab955aac9a64e02714054a2f430b4
Sunil Gunisetty <sgunisetty@2u.com>
2019-02-27 10:39:41 -0500

    Stop shadowing name

--- src/net/eraserhead/clara_eql.clj
+++ src/net/eraserhead/clara_eql.clj
@@ -6,17 +6,17 @@
 
 (defrecord QueryData [query root data])
 
-(defmacro defrule [name & body]
+(defmacro defrule [rule-name & body]
   (assert (= :query (first body)) "rule must start with :query")
   (let [query (second body)
         _ (assert (= :from (nth body 2)) "rule missing :from in third position")
         from (nth body 3)
         _ (assert (symbol? from) ":from value is not a symbol")
         where (drop 5 body)
-        qualified-name (symbol (clojure.core/name (ns-name *ns*)) (clojure.core/name name))
+        qualified-name (symbol (name (ns-name *ns*)) (name rule-name))
         rule-body (concat
                    where
                    ['=>
                     `(r/insert! (->QueryData '~qualified-name ~from {}))])]
-    `(def ~(vary-meta name assoc :rule true)
-       ~(dsl/build-rule name rule-body (meta &form)))))
+    `(def ~(vary-meta rule-name assoc :rule true)
+       ~(dsl/build-rule rule-name rule-body (meta &form)))))
