ac198132cabb342a5c84300eecfb604b232527db
Jason Felice <jason.m.felice@gmail.com>
2019-03-06 13:17:57 -0500

    Make dissoc-in private

--- src/net/eraserhead/clara_eql/eav_map.clj
+++ src/net/eraserhead/clara_eql/eav_map.clj
@@ -15,24 +15,17 @@
     (keyword (namespace k) (subs (name k) 1))
     (keyword (namespace k) (str "_" (name k)))))
 
-(defn dissoc-in
-  "Dissociate a value in a nested assocative structure, identified by a sequence
-  of keys. Any collections left empty by the operation will be dissociated from
-  their containing structures.
-  Taken directly from medley core"
-  ([m ks]
-   (if-let [[k & ks] (seq ks)]
-     (if (seq ks)
-       (let [v (dissoc-in (get m k) ks)]
-         (if (empty? v)
-           (dissoc m k)
-           (assoc m k v)))
-       (dissoc m k))
-     m))
-  ([m ks & kss]
-   (if-let [[ks' & kss] (seq kss)]
-     (recur (dissoc-in m ks) ks' kss)
-     (dissoc-in m ks))))
+;; Taken from medley core
+(defn- dissoc-in
+  [m ks]
+  (if-let [[k & ks] (seq ks)]
+    (if (seq ks)
+      (let [v (dissoc-in (get m k) ks)]
+        (if (empty? v)
+          (dissoc m k)
+          (assoc m k v)))
+      (dissoc m k))
+    m))
 
 (defn- reduce-fn
   [acc {:keys [e a v] :as datom}]
