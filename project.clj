ca90901774d8fc5fccbac102e21757e510092f3a
Jason Felice <jason.m.felice@gmail.com>
2019-03-14 14:41:54 -0400

    Set up release tasks

--- project.clj
+++ project.clj
@@ -3,6 +3,17 @@
   :url "https://github.com/eraserhd/clara-eql.git"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password}]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clyfe/clara-eav "0.1.6"]
                  [com.cerner/clara-rules "0.19.0"]
