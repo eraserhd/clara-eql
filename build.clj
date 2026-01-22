40a112a352e1b1db68e8f7bcefa90502faaeef85
Jason Felice <jason.m.felice@gmail.com>
2026-01-22 12:55:17 -0500

    Prepare for more work

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
    [deps-deploy.deps-deploy :as d]))
 
 (def lib 'net.eraserhead/clara-eql)
-(def version "0.2.0")
+(def version "0.2.1-SNAPSHOT")
 (def class-dir "target/classes")
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
 
