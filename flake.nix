3356ecea01694e3b6418a53ece7d803dafa35dc3
Jason Felice <jason.m.felice@gmail.com>
2026-01-22 10:35:55 -0500

    nix flake check works

--- flake.nix
+++ flake.nix
@@ -8,12 +8,41 @@
     (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        # `deps.nix` is generated with:
+        #
+        #     $ nix run github:hlolli/clj2nix -- deps.edn deps.nix -A:test -A:cljs-test
+        #
+        cljdeps = import ./deps.nix {
+          inherit (pkgs) fetchMavenArtifact fetchgit lib;
+        };
+        classpath = cljdeps.makeClasspaths {
+          extraClasspaths = [ "./src" "./test" ];
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             clojure
             nodejs_24
           ];
+        };
+        checks = {
+          tests = pkgs.stdenv.mkDerivation {
+            name = "clara-eql-tests";
+            src = ./.;
+            buildInputs = with pkgs; [
+              git
+              clojure
+              nodejs_24
+            ];
+            buildPhase = ''
+              set -x
+              mkdir tmp
+              HOME=tmp clojure -Scp ${classpath} -M:test
+              HOME=tmp clojure -Scp ${classpath} -M:cljs-test
+            '';
+            installPhase = "touch $out";
+          };
         };
     }));
 }
